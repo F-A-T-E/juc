85a103339f5ec47cb58e7440840726ab78632845
lvpf <1359706026@qq.com>
2021-09-09 16:44:27 +0800

    111

---
+++ src/main/java/com/lvpf/juc/c_025/T08_SynchronusQueue.java
@@ -0,0 +1,20 @@
+package com.lvpf.juc.c_025;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+
+public class T08_SynchronusQueue {
+	public static void main(String[] args) throws InterruptedException {
+		BlockingQueue<String> strs = new SynchronousQueue<>();
+
+		new Thread(()->{
+			try{
+				System.out.println(strs.take());
+			}catch (InterruptedException e){
+				e.printStackTrace();
+			}
+		}).start();
+		strs.put("aaa");
+		System.out.println(strs.size());
+	}
+}
