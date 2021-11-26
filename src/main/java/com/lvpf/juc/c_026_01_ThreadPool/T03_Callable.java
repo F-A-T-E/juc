c118f4257f0c16a06e8a2d93a3fa4062ddacec17
lvpf <1359706026@qq.com>
2021-11-26 17:50:12 +0800

    111

--- src/main/java/com/lvpf/juc/c_026_01_ThreadPool/T03_Callable.java
+++ src/main/java/com/lvpf/juc/c_026_01_ThreadPool/T03_Callable.java
@@ -1,4 +1,21 @@
 package com.lvpf.juc.c_026_01_ThreadPool;
 
+import java.util.concurrent.*;
+
 public class T03_Callable {
+	public static void main(String[] args) throws ExecutionException, InterruptedException {
+		Callable<String> c = new Callable() {
+			@Override
+			public String call() throws Exception {
+				return "Hello Callable";
+			}
+		};
+
+		ExecutorService service = Executors.newCachedThreadPool();
+		Future<String> future = service.submit(c);//异步
+
+		System.out.println(future.get());//阻塞
+
+		service.shutdown();
+	}
 }
---
+++ src/main/java/com/lvpf/juc/c_026_01_ThreadPool/T06_00_Future.java
@@ -0,0 +1,26 @@
+package com.lvpf.juc.c_026_01_ThreadPool;
+
+import java.util.concurrent.*;
+
+public class T06_00_Future {
+	public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+		FutureTask<Integer> task = new FutureTask<>(()->{
+			TimeUnit.MILLISECONDS.sleep(500);
+			return 1000;
+		}); // new Callable(){Integer call();}
+
+		new Thread(task).start();
+
+		System.out.println(task.get());//阻塞
+
+		//***************************
+		ExecutorService service = Executors.newFixedThreadPool(5);
+		Future<Integer> f = service.submit(()->{
+			TimeUnit.MILLISECONDS.sleep(500);
+			return 1;
+		});
+		System.out.println(f.get());
+		System.out.println(f.isDone());
+	}
+}
