4640cb3ebc9eeb1f4bacf8e60a7fa537673b9131
lvpf <1359706026@qq.com>
2021-11-26 20:27:09 +0800

    111

--- src/main/java/com/lvpf/juc/c_026_01_ThreadPool/T06_00_Future.java
+++ src/main/java/com/lvpf/juc/c_026_01_ThreadPool/T06_00_Future.java
@@ -5,22 +5,23 @@ import java.util.concurrent.*;
 public class T06_00_Future {
 	public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-		FutureTask<Integer> task = new FutureTask<>(()->{
+		FutureTask<Integer> task = new FutureTask<>(() -> {
 			TimeUnit.MILLISECONDS.sleep(500);
 			return 1000;
-		}); // new Callable(){Integer call();}
+		});
+		// new Callable(){Integer call();}
 
 		new Thread(task).start();
 
 		System.out.println(task.get());//阻塞
 
-		//***************************
-		ExecutorService service = Executors.newFixedThreadPool(5);
-		Future<Integer> f = service.submit(()->{
-			TimeUnit.MILLISECONDS.sleep(500);
-			return 1;
-		});
-		System.out.println(f.get());
-		System.out.println(f.isDone());
+			 //***************************
+			 ExecutorService service = Executors.newFixedThreadPool(5);
+			 Future<Integer> f = service.submit(()->{
+			 TimeUnit.MILLISECONDS.sleep(500);
+			 return 1;
+			 });
+			 System.out.println(f.get());
+			 System.out.println(f.isDone());
+			 }
 	}
-}
